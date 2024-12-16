9ab5a3529a8436dc98094d43cd765a8c86a585dc
Diego de Oliveira <diegohkd@gmail.com>
2024-12-16 20:26:02 -0300

    Update dependencies and remove unused files

--- buildSrc/src/main/kotlin/BuildConstants.kt
+++ buildSrc/src/main/kotlin/BuildConstants.kt
@@ -1,7 +1,7 @@
 object BuildConstants {
     const val MIN_SDK = 24
-    const val TARGET_SDK = 34
-    const val COMPILE_SDK = 34
+    const val TARGET_SDK = 35
+    const val COMPILE_SDK = 35
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0"
 }
